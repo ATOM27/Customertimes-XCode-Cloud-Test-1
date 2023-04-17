d0f2e7ddfbc728b9a2c4aeb21b0370bfac650b7b
Yevhen Mekhedov <yevhen.mekhedov@customertimes.com>
2023-04-17 11:12:43 +0300

    initial commit

---
+++ Customertimes Xcode Cloud Test 1/Customertimes_Xcode_Cloud_Test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Customertimes_Xcode_Cloud_Test_1App.swift
+//  Customertimes Xcode Cloud Test 1
+//
+//  Created by Yevhen Mekhedov on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Customertimes_Xcode_Cloud_Test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
